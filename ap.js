78497b32a2c6105de0aa70a04ec38b59bb05744e
haupvpd08216 <127080881+haupvpd08216@users.noreply.github.com>
2023-03-11 17:24:44 +0700

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,9 @@
+let a = prompt("moi ban nhap so nguyen a:");
+//let a1= parseInt(a);
+
+let b = prompt("moi ban nhap so nguyen b");
+//let b1 = parseInt(b);
+
+let c = parseInt(a)+parseInt(b)
+//let c = a + b;
+document.write(c);
